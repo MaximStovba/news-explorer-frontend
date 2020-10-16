c6ede0fb8bdeea6427dff0784e89f8712bb43555
Maxim Stovba <maxim.stovba@gmail.com>
2020-10-16 21:54:48 +0300

    Установлены шрифты

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from '../Header/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>News Explorer!</h1>
+    <div className="app">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+// Header.js
+
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Header News Explorer!</h1>
+    </header>
+  );
+}
+
+export default Header;
