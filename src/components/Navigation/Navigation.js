b69d7d98eafaa35fe33939490dab43c1e6d12e44
Maxim Stovba <maxim.stovba@gmail.com>
2020-10-18 00:42:52 +0300

    компонент Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,11 +5,11 @@ import './Navigation.css';
 
 function Navigation() {
   return (
-    <div className="navigation navigation_status_signin">
-      <h2 className="navigation__link navigation__link_type_main">Главная</h2>
-      <h2 className="navigation__link navigation__link_type_article">Сохранённые&nbsp;статьи</h2>
-      <h2 className="navigation__btn">Авторизоваться</h2>
-    </div>
+    <nav className="navigation navigation_status_signin">
+      <a href="/" className="navigation__link navigation__link_type_main navigation__link_status_active">Главная</a>
+      <a href="/" className="navigation__link navigation__link_type_article">Сохранённые&nbsp;статьи</a>
+      <button type="button" className="navigation__btn navigation__btn_logout">Авторизоваться</button>
+    </nav>
   );
 }
 
