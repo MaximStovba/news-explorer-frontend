12ccfcf78e5a40b4650ed296fdb1aae6bf8a16a8
Maxim Stovba <maxim.stovba@gmail.com>
2020-10-18 21:24:34 +0300

    компонент About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,9 +5,14 @@ import './About.css';
 
 function About() {
   return (
-    <article className="about">
-      <h1>About!</h1>
-    </article>
+    <section className="about">
+      <h2 className="about__title">Об авторе</h2>
+      <article className="about__article">
+        <p className="about__text">Это блок с описанием автора проекта. Здесь следует указать, как вас зовут, чем вы занимаетесь, какими технологиями разработки владеете.</p>
+        <p className="about__text">Также можно рассказать о процессе обучения в Практикуме, чему вы тут научились, и чем можете помочь потенциальным заказчикам.</p>
+      </article>
+      <div className="about__img-container"></div>
+    </section>
   );
 }
 
