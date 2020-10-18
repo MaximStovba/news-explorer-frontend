707ed1798e47d83a5c3ca97d4c875381f0e3e73c
Maxim Stovba <maxim.stovba@gmail.com>
2020-10-18 19:14:13 +0300

    компонент SearchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,8 +5,24 @@ import './SearchForm.css';
 
 function SearchForm() {
   return (
-    <form className="search-form">
-      
+    <form name="search" method="POST" action="#" className="search-form" noValidate>
+      <h2 className="search-form__title">Что творится в мире?</h2>
+      <p className="search-form__text">Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.</p>
+      <input
+        type="text"
+        name="search"
+        placeholder="Введите тему новости"
+        id="search-input"
+        className="search-form__input"
+        minLength="2"
+        maxLength="30"
+        required
+      />
+      <button
+          type="submit"
+          className="search-form__btn">
+          Искать
+      </button>
     </form>
   );
 }
