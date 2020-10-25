a4d12d4e82994f4c7ceaea65578acf595b42069a
Maxim Stovba <maxim.stovba@gmail.com>
2020-10-26 00:34:38 +0300

    резина NewsCardList

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,12 +6,14 @@ import './About.css';
 function About() {
   return (
     <section className="about">
-      <h2 className="about__title">Об авторе</h2>
-      <article className="about__article">
-        <p className="about__text">Это блок с описанием автора проекта. Здесь следует указать, как вас зовут, чем вы занимаетесь, какими технологиями разработки владеете.</p>
-        <p className="about__text">Также можно рассказать о процессе обучения в Практикуме, чему вы тут научились, и чем можете помочь потенциальным заказчикам.</p>
-      </article>
-      <div className="about__img-container"></div>
+      <div className="about__container">
+        <h2 className="about__title">Об авторе</h2>
+        <article className="about__article">
+          <p className="about__text">Это блок с описанием автора проекта. Здесь следует указать, как вас зовут, чем вы занимаетесь, какими технологиями разработки владеете.</p>
+          <p className="about__text">Также можно рассказать о процессе обучения в Практикуме, чему вы тут научились, и чем можете помочь потенциальным заказчикам.</p>
+        </article>
+        <div className="about__img-container"></div>
+      </div>
     </section>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,10 +4,10 @@ import React from 'react';
 import Header from '../Header/Header';
 import SearchForm from '../SearchForm/SearchForm';
 import NewsCardList from '../NewsCardList/NewsCardList';
-//import About from '../About/About';
+import About from '../About/About';
 //import Footer from '../Footer/Footer';
 //import Preloader from '../Preloader/Preloader';
-// import NotFound from '../NotFound/NotFound';
+//import NotFound from '../NotFound/NotFound';
 import './Main.css';
 
 const isMain = true;
@@ -18,6 +18,7 @@ function Main( {loggedIn, handleLogInClick} ) {
       <Header loggedIn={loggedIn} isMain={isMain} handleLogInClick={handleLogInClick} />
       <SearchForm />
       <NewsCardList loggedIn={loggedIn} isMain={isMain} />
+      <About />
 {/*
       <About />
       <Preloader />
