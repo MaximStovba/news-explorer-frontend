f5d4d811050541e7ebb2df271e729087b845d047
Maxim Stovba <maxim.stovba@gmail.com>
2020-10-25 20:57:28 +0300

    резина NewsCardList

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import Header from '../Header/Header';
 import SearchForm from '../SearchForm/SearchForm';
-//import NewsCardList from '../NewsCardList/NewsCardList';
+import NewsCardList from '../NewsCardList/NewsCardList';
 //import About from '../About/About';
 //import Footer from '../Footer/Footer';
 //import Preloader from '../Preloader/Preloader';
@@ -17,9 +17,8 @@ function Main( {loggedIn, handleLogInClick} ) {
     <div className="main">
       <Header loggedIn={loggedIn} isMain={isMain} handleLogInClick={handleLogInClick} />
       <SearchForm />
-
-{/*
       <NewsCardList loggedIn={loggedIn} isMain={isMain} />
+{/*
       <About />
       <Preloader />
       <NotFound />
