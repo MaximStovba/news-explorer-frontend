622d78748e1287e03f366d83feb2ea519e70d4f9
Maxim Stovba <maxim.stovba@gmail.com>
2020-11-03 16:02:23 +0300

    btn Show more

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -6,23 +6,46 @@ import './NewsCardList.css';
 
 function NewsCardList({ loggedIn, isMain, handleLogInClick, cards, question }) {
 
-  const isLiked = false;
+  const isLiked = false; // убрать
+  // счетчик
+  const [increment, setIncrement] = React.useState(3);
+  // показ кнопки
+  const [isBtnVisible, setIsBtnVisible] = React.useState(true);
+  // массив карт для показа
+  const [cardsToShow, setCardsToShow] = React.useState([]);
+
+  React.useEffect(() => {
+    if (increment >= cards.length) {
+      setIsBtnVisible(false); // скрываем кнопку
+    }
+    if (cards.length > 3) {
+      setCardsToShow(cards.slice(0, increment));
+    } else {
+      setCardsToShow(cards.slice(0));
+    }
+  }, [increment, cards]);
+
+  // обработчик нажатия кнопки "показать еще"
+  function handleBtnShowMoreClick() {
+    // увеличиваем счетчик
+    setIncrement(increment + 3);
+  }
 
   return (
     <form
       name="cardlist"
       method="POST"
       action="/"
-      className={`news-cardlist news-cardlist_status_${loggedIn ? 'login' : 'logout'}`}>
+      className={`news-cardlist news-cardlist_status_${!isBtnVisible ? 'login' : 'logout'}`}>
 
       <h2 className={
        `news-cardlist__titel
-        news-cardlist__titel_status_${loggedIn ? 'login' : 'logout'}
+        news-cardlist__titel_status_${!isBtnVisible ? 'login' : 'logout'}
        `}>Результаты поиска
       </h2>
-      <div className={`news-cardlist__container news-cardlist__container_status_${loggedIn ? 'login' : 'logout'}`}>
+      <div className={`news-cardlist__container news-cardlist__container_status_${!isBtnVisible ? 'login' : 'logout'}`}>
         {
-          cards.map(item => <NewsCard key={Math.random().toString(36).substr(2, 9)}
+          cardsToShow.map(item => <NewsCard key={Math.random().toString(36).substr(2, 9)}
             card={item}
             loggedIn={loggedIn}
             isLiked={isLiked}
@@ -33,9 +56,10 @@ function NewsCardList({ loggedIn, isMain, handleLogInClick, cards, question }) {
       </div>
       <button
           type="button"
+          onClick={handleBtnShowMoreClick}
           className={
          `news-cardlist__btn
-          news-cardlist__btn_status_${loggedIn ? 'login' : 'logout'}
+          news-cardlist__btn_status_${!isBtnVisible ? 'login' : 'logout'}
          `}>Показать еще
       </button>
     </form>
