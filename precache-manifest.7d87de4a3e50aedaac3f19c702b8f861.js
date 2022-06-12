31166cb743a9ab33c667597d82e3379e5d164eeb
MaximStovba <61358072+MaximStovba@users.noreply.github.com>
2022-06-12 23:44:58 +0300

    Updates

---
+++ precache-manifest.7d87de4a3e50aedaac3f19c702b8f861.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b27662fa851c68134b2624dba2afb837",
+    "url": "/news-explorer-frontend/index.html"
+  },
+  {
+    "revision": "f2ad0c8deb5068a27fef",
+    "url": "/news-explorer-frontend/static/css/main.e9282e67.chunk.css"
+  },
+  {
+    "revision": "c669a3176923492190b5",
+    "url": "/news-explorer-frontend/static/js/2.a1af9f0f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/news-explorer-frontend/static/js/2.a1af9f0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2ad0c8deb5068a27fef",
+    "url": "/news-explorer-frontend/static/js/main.c7df4c9a.chunk.js"
+  },
+  {
+    "revision": "30563db5395ebfdbb83c",
+    "url": "/news-explorer-frontend/static/js/runtime-main.0fc9bfae.js"
+  },
+  {
+    "revision": "12d6899432985ace8febf3318c96cd29",
+    "url": "/news-explorer-frontend/static/media/about-img.12d68994.jpeg"
+  },
+  {
+    "revision": "dfcf8893f85887d36e2ca860f4037eec",
+    "url": "/news-explorer-frontend/static/media/btn_like_hover.dfcf8893.svg"
+  },
+  {
+    "revision": "d69d34e7f89dab6097e24ae96e07f96a",
+    "url": "/news-explorer-frontend/static/media/btn_like_marked.d69d34e7.svg"
+  },
+  {
+    "revision": "fca692d493c8c85a5a3a9eb0d1ee149f",
+    "url": "/news-explorer-frontend/static/media/btn_like_normal.fca692d4.svg"
+  },
+  {
+    "revision": "b49fec91434aa5a1c7618dab75d41ee6",
+    "url": "/news-explorer-frontend/static/media/btn_trash_dsbl.b49fec91.svg"
+  },
+  {
+    "revision": "bf508e44493133bae384d57d4435357d",
+    "url": "/news-explorer-frontend/static/media/btn_trash_enbl.bf508e44.svg"
+  },
+  {
+    "revision": "f905582d6341bcace722b772bd80a31d",
+    "url": "/news-explorer-frontend/static/media/close-icon.f905582d.svg"
+  },
+  {
+    "revision": "c5d3f2ae48f9993aa89518ea2b312a23",
+    "url": "/news-explorer-frontend/static/media/fb.c5d3f2ae.svg"
+  },
+  {
+    "revision": "68b666c20cbb64099fe546112fa4fbea",
+    "url": "/news-explorer-frontend/static/media/github.68b666c2.svg"
+  },
+  {
+    "revision": "fc193a75f3eaf0f876378925f644ee56",
+    "url": "/news-explorer-frontend/static/media/icon_menu_close_white.fc193a75.svg"
+  },
+  {
+    "revision": "24c085fac85e2aa196e87d5aab33cfa2",
+    "url": "/news-explorer-frontend/static/media/icon_menu_open_black.24c085fa.svg"
+  },
+  {
+    "revision": "c366a189b6626cf62c1e024c8fb8c876",
+    "url": "/news-explorer-frontend/static/media/icon_menu_open_white.c366a189.svg"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "/news-explorer-frontend/static/media/inter-regular.3ae6a7d3.woff"
+  },
+  {
+    "revision": "c42e8502ca02b532e5c97f637b12f8b7",
+    "url": "/news-explorer-frontend/static/media/logout_black.c42e8502.svg"
+  },
+  {
+    "revision": "05f14eccb12baee416eff3dc3085dbba",
+    "url": "/news-explorer-frontend/static/media/logout_white.05f14ecc.svg"
+  },
+  {
+    "revision": "48a4c4c0b451e753e133551771e9fb40",
+    "url": "/news-explorer-frontend/static/media/not-found.48a4c4c0.svg"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/news-explorer-frontend/static/media/roboto-regular.58bf8e0b.woff"
+  },
+  {
+    "revision": "594bcf8bf5ddbb7302400b1319270585",
+    "url": "/news-explorer-frontend/static/media/robotoslab-regular.594bcf8b.woff"
+  },
+  {
+    "revision": "ca5573ca7d822890ddf45842af3a4fb1",
+    "url": "/news-explorer-frontend/static/media/search_bg.ca5573ca.png"
+  },
+  {
+    "revision": "49d8d8285e8bc3047dac602de489bfb0",
+    "url": "/news-explorer-frontend/static/media/sourcesanspro-regular.49d8d828.woff"
+  }
+]);
